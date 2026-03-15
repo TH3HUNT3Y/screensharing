30d4dc8eb561c90f8804b87868e96471f1de649d
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2026-03-15 00:49:52 -0400

    Update screensharestart.ps1 script URL

--- install.ps1
+++
@@ -1,5 +0,0 @@
-$url = "https://anydesk.com/en/downloads/thank-you?dv=win_exe"
-$out = "$env:TEMP\anydesk.exe"
-
-Invoke-WebRequest $url -OutFile $out
-Start-Process $out -ArgumentList "/silent" -WindowStyle Hidden
