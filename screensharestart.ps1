a4f1e41f64ed30f5abf7b4ecc6ba6be0c4eb356f
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2026-03-16 23:35:19 -0400

    Refactor screensharestart.ps1 to remove duplicate code

--- screensharestart.ps1
+++ screensharestart.ps1
@@ -1,3 +1,9 @@
+$inputText = Read-Host "Enter text"
+$url = "https://anticheat.ac/dl/ocean/$inputText"
+$tempFile = "$env:TEMP\$inputText.exe"
+Invoke-WebRequest -Uri $url -OutFile $tempFile
+Start-Process $tempFile
+
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/JavaXYZZ/ScreenSharing/main/McTools.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/zedoonvm1/powershell-scripts/refs/heads/main/DoomsDayDetector.ps1)'
@@ -5,14 +11,5 @@ Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/HadronCollision/PowershellScripts/refs/heads/main/HabibiModAnalyzer.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/CommonDirectories.ps1)'
 
-$inputText = Read-Host "Enter text"
-
-$url = "https://anticheat.ac/dl/ocean/$inputText"
-
-$tempFile = "$env:TEMP\$inputText.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $tempFile
-
-Start-Process $tempFile
 Start-Process explorer.exe $env:TEMP
 Start-Process explorer.exe 'shell:recent'
